cbf278dfba41d396d1efb47334a8081f40b19428
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-17 12:04:38 +0530

    refactor:while condition to calculate daily wage

--- Employee_Wage_Computation.java
+++ Employee_Wage_Computation.java
@@ -1,38 +1,41 @@
- 
 public class EmployeeWage
 {
 
 public static void main(String args[]){
-System.out.println("*********Welcome To Employee Wage System**********");
 		//constant
 		final int fullTime = 1;
 		final int partTime = 2;
 		final int wagePerHour = 20;
 		final int fullDayHour = 8;
+		final int dayPerMonth = 20;
+		final int totalHours = 100;
 		//variable
 		int dailyWage;
 		int dailyHours;
+		int monthlyWage = 0;
+		int hours = 0;
+		int day = 0;
 		//computation
-		for ( int day=0 ; day<=dayPerMonth ; day++ )
+		while ( hours <= totalHours && day <= dayPerMonth )
 		{
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case fullTime:
-				dailyHours = fullDayHour;
-				break;
-			case partTime:
-				dailyHours = fullDayHour / 2;
-				break;
-			default:
-				dailyHours = 0;
-				break;
-		}
-		//Calculating Monthly Wage
-		dailyWage = wagePerHour * dailyHours;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case fullTime:
+					dailyHours = fullDayHour;
+					break;
+				case partTime:
+					dailyHours = fullDayHour / 2;
+					break;
+				default:
+					dailyHours = 0;
+					break;
+			}
+			hours += dailyHours;
+			day++;
+			dailyWage = wagePerHour * dailyHours;
 			monthlyWage += dailyWage;
 		}
-		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));
-		System.out.println("Employee monthly wage is "+monthlyWage);
+		System.out.println("Employee monthly wage is "+monthlyWage+ " : "+hours+" : "+day);
 }
 
 }
